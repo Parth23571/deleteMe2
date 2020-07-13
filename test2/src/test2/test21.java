c2a12b7ab68795e0696ba8287655359d0a94f26f
spselenim <pawal.satish@gmail.com>
2020-07-13 19:03:17 +0530

    AC1

--- test2/src/test2/test21.java
+++ test2/src/test2/test21.java
@@ -3,7 +3,7 @@ package test2;
 public class test21 {
 
 	public static void main(String[] args) {
-System.out.println("in 21 >> Changed >> Another Change");
+System.out.println("in 21 >> Changed >> Another Change1");
 	}
 
 }
